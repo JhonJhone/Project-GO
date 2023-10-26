5d4391e1ed72ded504847de6c702d5dc8914a984
JhonJhone <126169393+JhonJhone@users.noreply.github.com>
2023-10-26 15:04:48 -0300

    starting login

--- login.go
+++ login.go
@@ -9,9 +9,17 @@ import (
 
 
 func Register(r *http.Request, w http.ResponseWriter) {
+	database.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
 
 }
 
 func Login(r *http.Request, w http.ResponseWriter) {
+	database.ExecuteTemplate(w, "Login", nil)
+}
+
+func Validate(w http.ResponseWriter, r *http.Request) {
 	
 }
